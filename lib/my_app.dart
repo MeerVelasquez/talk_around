3f050e870ba761c2c96c83c573bad669816b2f6a
Eliana Del Mar Velásquez Rodriguez <103125856+MeerVelasquez@users.noreply.github.com>
2023-11-04 18:17:48 -0500

    First Page Interface
    First Page Interface

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -3,6 +3,19 @@
 import 'package:flutter/material.dart';
 import 'package:talk_around/ui/pages/first_page.dart';
 
+MaterialColor myPrimarySwatch = MaterialColor(0xFF997AC1, {
+  50: Color(0xFFF5EEF7),
+  100: Color(0xFFE4D6E5),
+  200: Color(0xFFD3BED4),
+  300: Color(0xFFC2A6C4),
+  400: Color(0xFFB18EB3),
+  500: Color(0xFF997AC1), // Este es el tono principal
+  600: Color(0xFF8A6CB0),
+  700: Color(0xFF7C5FA0),
+  800: Color(0xFF6D5190),
+  900: Color(0xFF5F4480),
+});
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -12,8 +25,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Talk Around',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: myPrimarySwatch,
+        primaryColor: Color(0x013E6A),
       ),
       home: const FirstPage(),
     );
--- lib/ui/pages/first_page.dart
+++ lib/ui/pages/first_page.dart
@@ -20,9 +20,49 @@ class _FirstPageState extends State<FirstPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        // child: Text('TalkAround'),
-        child: Image.asset('assets/logo.png'),
+      backgroundColor: Color(0xF2F2F2).withOpacity(1),
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/fondo.png',
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/logo.png',
+                  width: 360,
+                  height: 365,
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(height: 25),
+                Text(
+                  'Chat with Locals',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Anywhere, anytime',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
