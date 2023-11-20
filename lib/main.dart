1b65e2f6eb7cb46814aeab50f293d4b6465184c2
Leonardo Vergara Márquez <leonardo11vergara11@gmail.com>
2023-11-20 17:15:50 -0500

    config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:talk_around/my_app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+  } catch (err) {
+    print(err);
+  }
   runApp(const MyApp());
 }
